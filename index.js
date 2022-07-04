e76a3f556011de466946f4b439a5f93922af6bc7
Anirban Singha Roy <anirban.singha.roy@dnb.no>
2022-07-04 11:37:21 +0200

    Add few endpoints

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var express = require("express");
 var app = express();
 
 const port = parseInt(process.env.PORT) || 8080;
+const pod_name = process.env.HOSTNAME;
+app.get('/helloworld', (req, res) => {
+  res.send('Hello World from pod: '+pod_name);
+});
+
+app.get('/hellomars', (req, res) => {
+  res.send('Hello Mars from pod: '+pod_name);
+});
+
 app.listen(port, () => {
   console.log(`App: listening on port ${port}`);
 });
